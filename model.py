360ab34f1d8f67d56394501d2ed9a56c15452d8c
mg343 <milu.garimella@gmail.com>
2022-07-27 08:28:39 -0400

    integrated keras with training data

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
-import torch
+from keras.applications.vgg19 import VGG19
+from keras.applications.vgg19 import preprocess_input
+from keras import Sequential
+from keras.layers import Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
 
-x=torch.rand(2, 3) 
 
-print(x) 
+#Load train and test set
+train_dir = 'Training Set'
+
+train_datagen = ImageDataGenerator(rescale=1.0/255, horizontal_flip=True, zoom_range=0.2,shear_range=0.2)
+train_generator = train_datagen.flow_from_directory(directory=train_dir,target_size=(128,128),class_mode='categorical',batch_size=32)

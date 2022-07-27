164d36cdbbb8531b064fa6fe31ba0102fbf87414
mg343 <milu.garimella@gmail.com>
2022-07-27 15:44:27 -0400

    manually created test and validation datasets

--- model.py
+++ model.py
@@ -7,6 +7,14 @@ from keras.preprocessing.image import ImageDataGenerator
 
 #Load train and test set
 train_dir = 'Training Set'
+test_dir = 'Test Set'
+val_dir = 'Validation Set'
 
 train_datagen = ImageDataGenerator(rescale=1.0/255, horizontal_flip=True, zoom_range=0.2,shear_range=0.2)
 train_generator = train_datagen.flow_from_directory(directory=train_dir,target_size=(128,128),class_mode='categorical',batch_size=32)
+
+val_datagen = ImageDataGenerator(rescale=1.0/255)
+val_generator = train_datagen.flow_from_directory(directory=val_dir,target_size=(128,128),class_mode='categorical',batch_size=32)
+
+test_datagen = ImageDataGenerator(rescale=1.0/255)
+test_generator = train_datagen.flow_from_directory(directory=val_dir,target_size=(128,128),class_mode='categorical',batch_size=32)

fcaa2dc446ba161b8558d274e71e338773e020bb
mg343 <milu.garimella@gmail.com>
2022-07-29 15:51:23 -0400

    created bounding box picture analyzation

--- camerahands.py
+++ camerahands.py
@@ -20,6 +20,32 @@ _, frame = cap.read()
 
 h, w, c = frame.shape
 
+# def bbox(pic):
+#     framergb = cv2.cvtColor(pic, cv2.COLOR_BGR2RGB)
+#     result = hands.process(framergb)
+#     hand_landmarks = result.multi_hand_landmarks
+#     if hand_landmarks:
+#         for handLMs in hand_landmarks:
+#             x_max = 0
+#             y_max = 0
+#             x_min = w
+#             y_min = h
+#             for lm in handLMs.landmark:
+#                 x, y = int(lm.x * w), int(lm.y * h)
+#                 if x > x_max:
+#                     x_max = x
+#                 if x < x_min:
+#                     x_min = x
+#                 if y > y_max:
+#                     y_max = y
+#                 if y < y_min:
+#                     y_min = y
+#             y_min -= 20
+#             y_max += 20
+#             x_min -= 20
+#             x_max += 20
+
+
 img_counter = 0
 analysisframe = ''
 letterpred = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'Space', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -36,7 +62,33 @@ while True:
         img_name = "opencv_frame_{}.png".format(img_counter)
         analysisframe = frame
         showframe = analysisframe
-        cv2.imwrite(img_name, frame)
+
+        framergbanalysis = cv2.cvtColor(analysisframe, cv2.COLOR_BGR2RGB)
+        resultanalysis = hands.process(framergbanalysis)
+        hand_landmarksanalysis = resultanalysis.multi_hand_landmarks
+        if hand_landmarksanalysis:
+            for handLMsanalysis in hand_landmarksanalysis:
+                x_max = 0
+                y_max = 0
+                x_min = w
+                y_min = h
+                for lmanalysis in handLMsanalysis.landmark:
+                    x, y = int(lmanalysis.x * w), int(lmanalysis.y * h)
+                    if x > x_max:
+                        x_max = x
+                    if x < x_min:
+                        x_min = x
+                    if y > y_max:
+                        y_max = y
+                    if y < y_min:
+                        y_min = y
+                y_min -= 20
+                y_max += 20
+                x_min -= 20
+                x_max += 20
+
+        analysisframe = analysisframe[y_min:y_max, x_min:x_max]
+        cv2.imwrite(img_name, analysisframe)
         cv2.imshow("Frame", showframe)
         analysisframe = cv2.resize(analysisframe,(128,128))
         analysisframe = np.reshape(analysisframe,[1,128,128,3])

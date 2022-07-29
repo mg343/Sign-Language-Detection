8b7192f38d9da97f526cbf3eb823e6ed8d7a1010
mg343 <milu.garimella@gmail.com>
2022-07-28 20:05:21 -0400

    completed image prediction analysis

--- camerahands.py
+++ camerahands.py
@@ -18,65 +18,58 @@ h, w, c = frame.shape
 
 img_counter = 0
 analysisframe = ''
-
+letterpred = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'Space', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 while True:
     _, frame = cap.read()
-    while img_counter < 1:
-        
-        k = cv2.waitKey(1)
-        if k%256 == 27:
-            # ESC pressed
-            print("Escape hit, closing...")
-            break
-        elif k%256 == 32:
-            # SPACE pressed
-            img_name = "opencv_frame_{}.png".format(img_counter)
-            analysisframe = frame
-            showframe = analysisframe
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-            img_counter += 1
 
-
-        framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        result = hands.process(framergb)
-        hand_landmarks = result.multi_hand_landmarks
-        if hand_landmarks:
-            for handLMs in hand_landmarks:
-                x_max = 0
-                y_max = 0
-                x_min = w
-                y_min = h
-                for lm in handLMs.landmark:
-                    x, y = int(lm.x * w), int(lm.y * h)
-                    if x > x_max:
-                        x_max = x
-                    if x < x_min:
-                        x_min = x
-                    if y > y_max:
-                        y_max = y
-                    if y < y_min:
-                        y_min = y
-                y_min -= 20
-                y_max += 20
-                x_min -= 20
-                x_max += 20
-                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-                mp_drawing.draw_landmarks(frame, handLMs, mphands.HAND_CONNECTIONS)
-        cv2.imshow("Frame", frame)
-    else:
-        if img_counter == 1:
-            cv2.imshow("Frame", showframe)
-            analysisframe = cv2.resize(analysisframe,(128,128))
-            analysisframe = np.reshape(analysisframe,[1,128,128,3])
-            analysisframe = analysisframe/255.0
-            prediction = model.predict(analysisframe)
-            print("Predicted Letter: ",prediction[0])
-            img_counter += 1
-        else:
-            print('Analysis Finished, Closing in 5 Seconds')
-            time.sleep(5)
-            break
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
         break
-    cap.release()
-    cv2.destroyAllWindows()
+    elif k%256 == 32:
+        # SPACE pressed
+        analysisframe = frame
+        showframe = analysisframe
+        cv2.imshow("Frame", showframe)
+        analysisframe = cv2.resize(analysisframe,(128,128))
+        analysisframe = np.reshape(analysisframe,[1,128,128,3])
+        analysisframe = analysisframe/255.0
+        prediction = model.predict(analysisframe)
+        predarray = np.array(prediction[0])
+        maxindex = np.argmax(predarray)
+        print("Predicted Letter: ", letterpred[maxindex])
+        print('Prediction Array: ', prediction[0])
+        print("Analysis complete, closing in 5 seconds...")
+        time.sleep(5)
+        break
+
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(framergb)
+    hand_landmarks = result.multi_hand_landmarks
+    if hand_landmarks:
+        for handLMs in hand_landmarks:
+            x_max = 0
+            y_max = 0
+            x_min = w
+            y_min = h
+            for lm in handLMs.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
+            y_min -= 20
+            y_max += 20
+            x_min -= 20
+            x_max += 20
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            mp_drawing.draw_landmarks(frame, handLMs, mphands.HAND_CONNECTIONS)
+    cv2.imshow("Frame", frame)
+
+cap.release()
+cv2.destroyAllWindows()
